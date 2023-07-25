63524c25494e02a8a1990eb3fe1125634fefa535
Krylov Ivan <krylovis@mchs.ru>
2023-07-25 10:17:58 +0300

    add findUserByCredentials

--- models/user.js
+++ models/user.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const UnauthorizedError = require('../custom-errors/UnauthorizedError');
 
 const {
   WRONG_EMAIL_FORMAT,
+  WRONG_EMAIL_OR_PASSWORD,
 } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
@@ -27,5 +31,20 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) throw new UnauthorizedError(WRONG_EMAIL_OR_PASSWORD);
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) throw new UnauthorizedError(WRONG_EMAIL_OR_PASSWORD);
+          return user;
+        });
+    });
+}
+
+userSchema.statics.findUserByCredentials = findUserByCredentials;
 
 module.exports = mongoose.model('user', userSchema);
