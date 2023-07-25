8db14fce4dc2d3c1aa4a3a65c1ecbbdd672a5476
Krylov Ivan <krylovis@mchs.ru>
2023-07-25 15:39:39 +0300

    fix app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 const express = require('express');
-
-const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+const {
+  PORT = 3001,
+  MONGO_URL = 'mongodb://localhost:27017',
+} = process.env;
+
+mongoose.connect(`${MONGO_URL}/mestodb`);
+
+app.use(express.json());
+app.use(routes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
