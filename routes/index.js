67cdd16d6924ba861c675819d09923edb96d97b1
Krylov Ivan <krylovis@mchs.ru>
2023-07-25 15:21:41 +0300

    add corsSettings

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@ const router = require('express').Router();
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const { requestLogger, errorLogger } = require('../middlewares/logger');
+const { corsSettings } = require('../middlewares/corsSettings');
 
 const authRoutes = require('./authRoutes');
 
 router.use(requestLogger);
 
+router.use(corsSettings);
 router.use(cookieParser());
+
 router.use('/', authRoutes);
 
 router.use(errorLogger);
