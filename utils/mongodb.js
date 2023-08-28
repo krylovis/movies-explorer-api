ec033b3330e07b0433c33d74a5d31e13f1ab7175
Ivan Krylov <kis.226@yandex.ru>
2023-08-28 22:39:15 +0300

    fix

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { MONGO_URL, BD_NAME, NODE_ENV } = process.env;
 
 const isProduction = NODE_ENV === 'production';
-const MONGOOSE_URL = isProduction ? MONGO_URL : 'mongodb://localhost:127.0.0.1';
+const MONGOOSE_URL = isProduction ? MONGO_URL : 'mongodb://127.0.0.1:27017';
 const MONGOOSE_BD = isProduction ? BD_NAME : 'bitfilmsdb';
 
 module.exports.MONGOOSE_FULL_URL = `${MONGOOSE_URL}/${MONGOOSE_BD}`;
